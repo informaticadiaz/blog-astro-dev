d5dbd9ce928e14d1cb5f30d76b030f9f68759b70
Ignacio Diaz <informatica.diaz@gmail.com>
2023-10-29 21:34:46 -0300

    add two post

--- _astro/index.c6da1b03.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./hoisted.1f25d9b5.js";async function o(){return t(()=>import("./index.99a67478.js"),["_astro/index.99a67478.js","_astro/render-template.09255677.js","_astro/hoisted.1f25d9b5.js"])}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
