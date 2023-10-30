4e8f9ff14a082d99b6eecbbfa5a755038a64ad21
Ignacio Diaz <informatica.diaz@gmail.com>
2023-10-29 21:46:46 -0300

    add

---
+++ _astro/index.7bf515ae.js
@@ -0,0 +1,19 @@
+const e="svg/index.md",s="post",o="svg",t=`
+## SVG Resource
+
+### [Flaticon](https://www.flaticon.es/)
+
+Descarga iconos y stickers gratis para tus proyectos. Recursos hechos por y para diseñadores. Formatos PNG, SVG, EPS, PSD y CSS.
+
+### [Freepik](https://www.freepik.es/)
+
+Diseña mejor y más rápido; millones de vectores, fotos, vídeos y mockups para convertir tus ideas en diseños de forma sencilla
+
+### [SVGrepo](https://www.svgrepo.com/)
+
+Search, explore and edit the best-fitting free icons or vectors for your projects using a wide variety vector library. Download free SVG vectors and icons for commercial use.
+`,r={title:"SVG Resources",description:"SVG, Iconos, Resources, SVG page, icon, resources.",publishDate:new Date(16986348e5),draft:!1,tags:["resources","svg"]},n={type:"content",filePath:"/home/ignacio/devhome/blog-dev-astro/src/content/post/svg/index.md",rawData:`
+title: "SVG Resources"
+description: "SVG, Iconos, Resources, SVG page, icon, resources."
+publishDate: "30 Oct 2023"
+tags: ["Resources", "SVG"]`};export{n as _internal,t as body,s as collection,r as data,e as id,o as slug};
---
+++ _astro/index.d6301548.js
@@ -0,0 +1,54 @@
+import{c as i,r as n,m as s}from"./render-template.179cea7a.js";import{u as a}from"./hoisted.42a25906.js";const t=`<h2 id="ai-coding-assistant">AI coding assistant</h2>
+<p>AI Tools Every Developer Needs To try</p>
+<h3 id="sourcegraph"><a href="https://about.sourcegraph.com/cody">Sourcegraph</a></h3>
+<p>Sourcegraph’s Cody is the next generation of Github Copilot. It can understand your entire codebase, code graphs, and company documentation.</p>
+<h3 id="mutable"><a href="https://mutable.ai/">Mutable</a></h3>
+<p>Mutable AI is engineered to work with various popular programing laguages and integrates with Visual Studio Code, Jupyter, and GitHub.</p>
+<h3 id="stepsize"><a href="https://stepsize.com/">Stepsize</a></h3>
+<p>Stepsize AI seamlessly integrates with tools like Slack, Jira, and GitHub, providing insightful overviews of your activities and offering strategic suggestions.</p>
+<h3 id="codiumai"><a href="https://codium.ai/">CodiumAI</a></h3>
+<p>CodiumAI serves as an AI test-writing assistant. It analyses your code, docstrings, and comments to suggest tests inteligently.</p>
+<h3 id="readable"><a href="http://readable.so">Readable</a></h3>
+<p>Readable AI automates the process of generating comments for your source code. It’s compatible with popular IDEs like VSCode &#x26; it can read most languages.</p>
+<h3 id="adrenaline"><a href="https://useadrenaline.com/">Adrenaline</a></h3>
+<p>Adrenaline identifies and rectifies coding error in quick and efficient way. It is designned to assist developers by diagnosing errors and offering solutions.</p>
+<h3 id="mintifle"><a href="http://writer.mintlifly.com">Mintifle</a></h3>
+<p>Mintifly servers as your team’s technical write. It reads and interprets your code, turning it into a clear, readble document.</p>
+<h3 id="what-the-diff"><a href="http://whatthediff.ai">What the Diff</a></h3>
+<p>What the diff AI is an AI-powered code review tool. It writes pull request descriptions, scrutinises pull requests, identifies potential risks, and more.</p>`,o={title:"IA Tool",description:"AI coding assistant,  AI Accelerated Software Development, AI suggested right inside your IDE.",publishDate:"28 Oct 2023",tags:["IA"],minutesRead:"1 min read"},r="/home/ignacio/devhome/blog-dev-astro/src/content/post/ia-tool/index.md",d=void 0;function f(){return`
+## AI coding assistant
+
+AI Tools Every Developer Needs To try
+
+### [Sourcegraph](https://about.sourcegraph.com/cody)
+
+Sourcegraph's Cody is the next generation of Github Copilot. It can understand your entire codebase, code graphs, and company documentation.
+
+### [Mutable](https://mutable.ai/)
+
+Mutable AI is engineered to work with various popular programing laguages and integrates with Visual Studio Code, Jupyter, and GitHub.
+
+### [Stepsize](https://stepsize.com/)
+
+Stepsize AI seamlessly integrates with tools like Slack, Jira, and GitHub, providing insightful overviews of your activities and offering strategic suggestions.
+
+### [CodiumAI](https://codium.ai/)
+
+CodiumAI serves as an AI test-writing assistant. It analyses your code, docstrings, and comments to suggest tests inteligently.
+
+### [Readable](http://readable.so)
+
+Readable AI automates the process of generating comments for your source code. It's compatible with popular IDEs like VSCode & it can read most languages.
+
+### [Adrenaline](https://useadrenaline.com/)
+
+Adrenaline identifies and rectifies coding error in quick and efficient way. It is designned to assist developers by diagnosing errors and offering solutions.
+
+### [Mintifle](http://writer.mintlifly.com)
+
+Mintifly servers as your team's technical write. It reads and interprets your code, turning it into a clear, readble document.
+
+### [What the Diff](<http://whatthediff.ai>)
+
+What the diff AI is an AI-powered code review tool. It writes pull request descriptions, scrutinises pull requests, identifies potential risks, and more.
+`}function m(){return t}function I(){return[{depth:2,slug:"ai-coding-assistant",text:"AI coding assistant"},{depth:3,slug:"sourcegraph",text:"Sourcegraph"},{depth:3,slug:"mutable",text:"Mutable"},{depth:3,slug:"stepsize",text:"Stepsize"},{depth:3,slug:"codiumai",text:"CodiumAI"},{depth:3,slug:"readable",text:"Readable"},{depth:3,slug:"adrenaline",text:"Adrenaline"},{depth:3,slug:"mintifle",text:"Mintifle"},{depth:3,slug:"what-the-diff",text:"What the Diff"}]}const y=i((l,u,p)=>{const{layout:c,...e}=o;return e.file=r,e.url=d,n`${s()}${a(t)}`});export{y as Content,m as compiledContent,y as default,r as file,o as frontmatter,I as getHeadings,f as rawContent,d as url};
